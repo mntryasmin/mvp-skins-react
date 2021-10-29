d25a0bc64920874213838b25e9079c81029bfb79
Christopher <christopherqueiroz@hotmail.com>
2021-10-29 18:15:09 -0300

    Produto(alteração na rota produto para teste)

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,11 +1,55 @@
 import React from 'react'
 import './Product.css'
+import {Row, Col, Breadcrumb} from "react-bootstrap";
+import Produto from "../../assets/images/PRODUTOS/luvas-de-especialista.png"
 
 function Product(props) {
 
     return(
         <>
-        <div>Product</div>
+        
+        <div className="div-produto">
+            {/* inicio produto */}
+            <Col xs={11} md={10} className="m-0">
+            
+                <Row className="justify-content-center">
+                <Breadcrumb>
+                    <Breadcrumb.Item href="http://localhost:3000/">Home</Breadcrumb.Item>
+                    <Breadcrumb.Item href="#">Categoria</Breadcrumb.Item>
+                    <Breadcrumb.Item href="#">Subcategoria</Breadcrumb.Item>
+                    <Breadcrumb.Item active>Produto</Breadcrumb.Item>
+                </Breadcrumb>
+                    
+                    
+                    <Col xs={10} className="container-produto my-3 p-0">
+                        <Col xs={12} md={7}>
+                            <div className="container-imagem">
+                                <img src={Produto} alt="luva de especialista" className="imagem-descricao"/>
+                            </div>
+                        </Col>
+                        <Col xs={12} md={5} className="informacao-produto">
+                            <div className="descricao">
+                                <div className="texto-descricao">ESCOPETA</div>
+                                <div className="texto-descricao">EXTERIOR: USAHDSDAS</div>
+                                <div className="texto-descricao">RARIDADE: USAHDSDAS</div>
+                                <div className="texto-nome-produto">MAG-7 | COTA DE MALHA</div>
+                                <div className="container-preco">
+                                    <div className="preco-descricao">R$ 49,90</div>
+                                    <button type="button" class="btn btn-primary btn-mvp btn-primario-mvp me-2">Comprar</button>
+                                </div>
+                                <div className="texto-descricao">Coleção: SUHAIHSADDAS</div>
+                                <div className="texto-descricao">Float: USAHDSDAS</div>
+                                
+                            </div>
+                        </Col>
+                    </Col>
+                    
+                </Row>
+            </Col>
+            {/* fim produto */}
+        </div>
+        
+        
         </>
     )
 }
