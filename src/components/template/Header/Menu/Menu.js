4b0f0734d3c11db24fa3fc3c751f240f0386c93b
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-11-08 10:59:23 -0300

    atualizaçao diaria

--- src/components/template/Header/Menu/Menu.js
+++ src/components/template/Header/Menu/Menu.js
@@ -2,12 +2,13 @@ import { Button, Offcanvas, ListGroup } from 'react-bootstrap'
 import React, { useState } from 'react';
 import BtnMenu from '../../../../assets/images/icones/icon-menu.png'
 
-function Menu() {
+function Menu(props) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const listCategories = props.listCategories
     return (
         <>
             <Button variant="link" onClick={handleShow} >
@@ -20,24 +21,7 @@ function Menu() {
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                     <ListGroup variant="flush">
-                        <ListGroup.Item>
-                            <a href="#" className="link-header">PISTOLAS</a>
-                        </ListGroup.Item>
-                        <ListGroup.Item >
-                            <a href="#" className="link-header">SUBMETRALHADORAS</a>
-                        </ListGroup.Item>
-                        <ListGroup.Item >
-                            <a href="#" className="link-header">RIFLES</a>
-                        </ListGroup.Item>
-                        <ListGroup.Item >
-                            <a href="#" className="link-header">PESADAS</a>
-                        </ListGroup.Item>
-                        <ListGroup.Item >
-                            <a href="#" className="link-header">FACAS</a>
-                        </ListGroup.Item>
-                        <ListGroup.Item >
-                            <a href="#" className="link-header">AGENTES</a>
-                        </ListGroup.Item>
+                        <ListCategories/>
                     </ListGroup>
                 </Offcanvas.Body>
             </Offcanvas>
