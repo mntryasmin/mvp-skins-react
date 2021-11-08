7e59009619cef6f164f3a3c9dd07019402870577
Christopher <christopherqueiroz@hotmail.com>
2021-11-08 15:46:36 -0300

    pagina produto

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Product.css'
 import {Row, Col, Breadcrumb} from "react-bootstrap";
 import Images from '../../components/micro/Images/Images';
+import Button from '../../components/micro/Button/Button';
 
 
 function Product(props) {
@@ -15,14 +16,16 @@ function Product(props) {
             <Col xs={11} md={10} className="m-0">
             
                 <Row className="justify-content-center">
-                <Breadcrumb>
-                    <Breadcrumb.Item href="http://localhost:3000/">Home</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#">Categoria</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#">Subcategoria</Breadcrumb.Item>
-                    <Breadcrumb.Item active>Produto</Breadcrumb.Item>
-                </Breadcrumb>
-                    
-                    
+                    <div className="mt-2">
+                    <Breadcrumb bsPrefix="">
+                        <Breadcrumb.Item href="http://localhost:3000/">Home</Breadcrumb.Item>
+                        <Breadcrumb.Item href="#">Categoria</Breadcrumb.Item>
+                        <Breadcrumb.Item href="#">Subcategoria</Breadcrumb.Item>
+                        <Breadcrumb.Item active>Produto</Breadcrumb.Item>
+                    </Breadcrumb>
+                    </div>
+                        
+                        
                     <Col xs={10} className="container-produto my-3 p-0">
                         <Col xs={12} md={7}>
                             <div className="container-imagem">
@@ -37,10 +40,9 @@ function Product(props) {
                                 <div className="texto-nome-produto">MAG-7 | COTA DE MALHA</div>
                                 <div className="container-preco">
                                     <div className="preco-descricao">R$ 49,90</div>
-                                    <Col xs={10} md={4}>
-                                        <button type="button" class="btn btn-primary btn-mvp btn-primario-mvp me-2">Comprar</button>
+                                    <Col xs={10}>
+                                        <Button label="COMPRAR" class="btn btn-primary btn-mvp btn-primario-mvp product-btn"></Button>
                                     </Col>
-                                    
                                 </div>
                                 <div className="texto-descricao">Coleção: SUHAIHSADDAS</div>
                                 <div className="texto-descricao">Float: USAHDSDAS</div>
