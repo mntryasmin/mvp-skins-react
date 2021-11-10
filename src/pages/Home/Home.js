f05884bddbb331a40f350c703f05aea5e51052fe
yasminmq <ymonteiroq@gmail.com>
2021-11-09 23:54:05 -0300

    Correções na responsividade dos cards de produto

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,31 +8,67 @@ import './Home.css'
 
 // PÁGINAS/COMPONENTES
 import Banners from '../../components/macro/Banners/Banner'
-import CarouselProducts from '../../components/macro/CarouselProducts/CarouselProducts'
-import Card from '../../components/micro/CardProduct/CardProduct'
+import CarouselProductsXs from '../../components/macro/CarouselProducts/CarouselProducts-xs'
+import CarouselProductsSm from '../../components/macro/CarouselProducts/CarouselProducts-sm'
+import CarouselProductsMd from '../../components/macro/CarouselProducts/CarouselProducts-md'
+import CarouselProductsLgXl from '../../components/macro/CarouselProducts/CarouselProducts-lg-xl'
 
 function Home(props) {
 
     return (
         <>
-            <Container fluid className="px-0 py-4 mx-0 home">
+            <Container fluid className="px-2 py-4 mx-0 home">
                 {/* BANNERS  */}
                 <p className="mt-0 mb-0 pt-3 pb-1 title-banner card-caption-mvp">Destaques</p>
-                <Card/>
                 <Banners />
 
                 <p className="mt-5 mb-0 title-banner card-caption-mvp">Eventos</p>
                 <Banners />
 
                 {/* CAROUSEL'S DE PRODUTOS  */}
-                <p className="mt-5 mb-0 title-carousel card-caption-mvp">Skins mais vendidas</p>
-                <CarouselProducts />
+                <Container className="p-0 m-0 home-xs">
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Skins mais vendidas</p>
+                    <CarouselProductsXs />
 
-                <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top armas</p>
-                <CarouselProducts/>
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top armas</p>
+                    <CarouselProductsXs />
 
-                <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top facas</p>
-                <CarouselProducts/>
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top facas</p>
+                    <CarouselProductsXs />
+                </Container>
+
+                <Container className="p-0 m-0 home-sm">
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Skins mais vendidas</p>
+                    <CarouselProductsSm />
+
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top armas</p>
+                    <CarouselProductsSm />
+
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top facas</p>
+                    <CarouselProductsSm />
+                </Container>
+
+                <Container className="p-0 m-0 home-md">
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Skins mais vendidas</p>
+                    <CarouselProductsMd />
+
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top armas</p>
+                    <CarouselProductsMd />
+
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top facas</p>
+                    <CarouselProductsMd />
+                </Container>
+
+                <Container className="p-0 m-0 home-respons-lg-xl">
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Skins mais vendidas</p>
+                    <CarouselProductsLgXl />
+
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top armas</p>
+                    <CarouselProductsLgXl />
+
+                    <p className="mt-5 mb-0 title-carousel card-caption-mvp">Top facas</p>
+                    <CarouselProductsLgXl />
+                </Container>
             </Container>
         </>
     )
