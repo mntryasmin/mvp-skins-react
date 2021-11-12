82c1ce0e889b3cb40bfe12f42c7630e4728e86f8
Christopher <christopherqueiroz@hotmail.com>
2021-11-12 14:07:50 -0300

    Ajuste Produto

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -31,70 +31,40 @@ export default function Product() {
 
     return (
         <>
-        
-        <div className="div-produto">
-            <Col xs={11} md={10} className="m-0">
-            <Title title="PRODUTO"/>
-                <Row className="justify-content-center">
-                <Breadcrumb>
-                    <Breadcrumb.Item href="http://localhost:3000/">Home</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#">Categoria</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#">Subcategoria</Breadcrumb.Item>
-                    <Breadcrumb.Item active>Produto</Breadcrumb.Item>
-                </Breadcrumb>
-                    
-                    
-                    <Col xs={10} className="container-produto my-3 p-0">
-                        <Col xs={12} md={7}>
-                            <div className="container-imagem">
-                                <img src={Product} alt="luva de especialista" className="imagem-descricao"/>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={5} className="informacao-produto">
-                            <div className="descricao">
-                                <div className="texto-descricao">ESCOPETA</div>
-                                <div className="texto-descricao">EXTERIOR: USAHDSDAS</div>
-                                <div className="texto-descricao">RARIDADE: USAHDSDAS</div>
-                                <div className="texto-nome-produto">MAG-7 | COTA DE MALHA</div>
-                                <div className="container-preco">
-                                    <div className="preco-descricao">R$ 49,90</div>
-                                    <button type="button" className="btn btn-primary btn-mvp btn-primario-mvp me-2">Comprar</button>
-                                   
-                                </div>
-                                <div className="texto-descricao">Coleção: SUHAIHSADDAS</div>
-                                <div className="texto-descricao">Float: USAHDSDAS</div>
-                                
-                            </div>
-                        </Col>
-                    </Col>
-                    
-                    <Breadcrumb bsPrefix="" className="mt-2">
-                        <Breadcrumb.Item href="http://localhost:3000/">Home</Breadcrumb.Item>
-                        <Breadcrumb.Item href="/category">Categoria</Breadcrumb.Item>
-                        <Breadcrumb.Item href="/category">Subcategoria</Breadcrumb.Item>
-                        <Breadcrumb.Item active>Produto</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <ProductContainer urlImagem="pistola-visoes-ancestrais">
-                        <ProductText category="PISTOLAS" class="product-text-category"/>
-                        <ProductText category="EXTERIOR :" description="Bem desgastada (Well-Worn)"/>
-                        <ProductText category="RARIDADE :" description="Azul"/>
-                        <ProductText description="Visões Ancestrais" class="product-text-name"/>
-                        <ProductPrice idProduto={id}/>
-                        <Button label="COMPRAR" class="btn btn-primary btn-mvp btn-primario-mvp mb-3"
-                        route="/cart" navigation/>
-                        <ProductText category="Coleção :" description="A Coleção Ancient"/>
-                        <ProductText category="Float :" description="0.44"/>
-                    </ProductContainer>
-                </Row>
-                <ProductText description="Veja também:" class="product-text-carousel"/>
-                <CarouselProducts/>
-                <ProductText description="Veja também:" class="product-text-carousel"/>
-                <CarouselProducts/>
-                <div className="mb-5"></div>
-            </Col>
-        </div>
-        
-        
+
+            <div className="div-produto content-container">
+                <Col xs={11} md={10} className="m-0">
+                    <Title title="PRODUTO" class="mt-5"/>
+                    <Row className="justify-content-center">
+                        <Breadcrumb bsPrefix="breadcrumb" className="mt-2">
+                            <Breadcrumb.Item href="/" className="product-breadcrumb">Home</Breadcrumb.Item>
+                            <Breadcrumb.Item href="/category" className="product-breadcrumb">Categoria</Breadcrumb.Item>
+                            <Breadcrumb.Item href="/category" className="product-breadcrumb">Subcategoria</Breadcrumb.Item>
+                            <Breadcrumb.Item active>Produto</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <ProductContainer urlImagem="pistola-visoes-ancestrais">
+                            <ProductText category="PISTOLAS" class="product-text-category" />
+                            <ProductText category="EXTERIOR :" description="Bem desgastada (Well-Worn)" />
+                            <ProductText category="RARIDADE :" description="Azul" class="mb-4"/>
+                            <ProductText description="Visões Ancestrais" class="product-text-name" />
+                            <ProductPrice idProduto={id} />
+                            <Button label="COMPRAR" class=" btn-primary-mvp p-2 mt-2 mb-5"
+                                route="/cart" navigation/>
+                            <ProductText category="Coleção :" description="A Coleção Ancient" class="mt-4"/>
+                            <ProductText category="Float :" description="0.44" />
+                            <ProductText category="ATENÇÃO :" description="Visões Ancestrais é uma customização 
+                            de Item para o jogo online CS:GO, podendo ser utilizada apenas dentro do jogo CS:GO" class="mt-4"/>
+                        </ProductContainer>
+                    </Row>
+                    <ProductText description="Veja também:" class="product-text-carousel" />
+                    <CarouselProducts />
+                    <ProductText description="Veja também:" class="product-text-carousel" />
+                    <CarouselProducts />
+                    <div className="mb-5"></div>
+                </Col>
+            </div>
+
+
         </>
     )
 
