3f61a0e41c35c266e984be3346cc26dbcb384ce3
Christopher <christopherqueiroz@hotmail.com>
2021-11-12 11:38:29 -0300

    Sucesso de compra, contato, links navbar header

--- src/pages/Success/Success.js
+++ src/pages/Success/Success.js
@@ -15,7 +15,7 @@ function Sucess(props) {
 
     return (
         <>
-            <Container fluid className="row m-0 py-5 px-0 success-container  content-container">
+            <Container fluid className="row m-0 py-5 px-0 success-container content-container">
 
                 <Row className="col-9 my-0 p-5 success justify-content-center">
                     <Row className="row py-1 sucessImg"><img src={iconSucess} /></Row>
@@ -24,10 +24,10 @@ function Sucess(props) {
                     <p className="py-3 px-3 sucess-text">O pedido {props.pedido} foi efetuado com sucesso. Após a confirmação do pagamento sua skin estará disponível em até 1 dia útil por meio de um Trade Link da Steam que você receberá em seu e-mail. </p>
 
                     <Row className="py-3 sucess-button">
-                        <Col md={6} className="success-button-dashboard">
+                        <Col md={6} lg={5} className="success-button-dashboard">
                             <Button route="/dashboard" label="Acompanhar pedido" class="btn-primary-mvp p-2 w-100" navigation/>
-                    '   </Col>
-                        <Col md={6} className="success-button-home">
+                        </Col>
+                        <Col md={6} lg={5} className="success-button-home">
                             <Button route="/" label="Voltar para a home" class="btn-primary-mvp p-2 w-100" navigation/>
                         </Col>
                     </Row>
