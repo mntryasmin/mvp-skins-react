538ac9eaf1776c9c345a34a85636f441785d319c
yasminmq <ymonteiroq@gmail.com>
2021-11-12 12:27:56 -0300

    Correções de erros encontrados na revisão

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -21,37 +21,44 @@ function Checkout(props) {
             <Container fluid className="row px-2 py-5 mx-0 checkout content-container">
                 <h1 className="mb-3 card-title-mvp checkout-title"> Checkout </h1>
 
-                <Col xs={12} sm={12} md={4} lg={4} xl={4} className="py-4 mx-1 checkout-containers checkout-respons">
+                <Col xs={12} sm={12} md={12} lg={4} xl={4} className="py-4 mx-1 checkout-containers checkout-request checkout-respons">
                     <h1 className="mb-3 card-caption-mvp checkout-title"> Resumo do pedido </h1>
-                    <Container>
-                        <Row>
+                    <Container >
+                        <Row className="px-2 checkout-list-items-scroll">
                             <Products />
                         </Row>
 
                         <Row className="p-2 mt-3 checkout-price-container">
-                            <Col className="col-3 checkout-price-title"><p> Produtos </p></Col>
-                            <Col className="col-9 checkout-price"><p> R$ 1.000,00</p></Col>
-                            <hr className="p-0 mx-0 my-2 checkout-price-line" />
-                            <Col className="col-3 checkout-price-title"><p> Desconto </p></Col>
-                            <Col className="col-9 checkout-price coupon-value"><p >R$ -100,00 </p></Col>
-                            <hr className="p-0 mx-0 my-2 checkout-price-line" />
-                            <Col className="col-3 checkout-price-title checkout-price-sum"><p> Total </p></Col>
-                            <Col className="col-9 checkout-price checkout-price-sum"><p> R$900,00 </p></Col>
+                            <Row className="my-1 py-1 checkout-price ">
+                                <p className="checkout-price-title"> Produtos </p>
+                                <p> R$ 1.000,00</p>
+                            </Row>
+
+                            <Row className="my-1 py-1 checkout-price checkout-line">
+                                <p className="checkout-price-title"> Desconto </p>
+                                <p> R$ -100,00</p>
+                            </Row>
+
+                            <Row className="my-1 py-1  checkout-price checkout-line">
+                                <p className="checkout-price-title"> Total </p>
+                                <p> R$ 900,00</p>
+                            </Row>
                         </Row>
                     </Container>
                 </Col>
 
 
-                <Col xs={12} sm={12} md={4} lg={4} xl={4} className="px-5 py-4 mx-1 checkout-containers checkout-respons">
+                <Col xs={12} sm={12} md={12} lg={4} xl={4} className="px-5 py-4 mx-1 checkout-containers checkout-respons">
                     <h1 className="mb-3 card-caption-mvp checkout-title"> Pagamento </h1>
                     <PaymentCreditCard />
                 </Col>
 
 
-                <Col xs={12} sm={12} md={3} lg={3} xl={3} className="py-4 px-4 mx-1 checkout-term checkout-containers checkout-respons">
+                <Col xs={12} sm={12} md={12} lg={3} xl={3} className="py-4 px-4 mx-1 checkout-term checkout-containers checkout-respons">
                     <h1 className="mb-3 card-caption-mvp checkout-title"> Termos de serviço </h1>
-                    <p className="pt-3">Eu estou ciente de que a após a troca terei que aguardar por 7 (sete) dias para
-                        realizar outra troca com a skin adquirida nessa transação.
+                    <p className="pt-3">Eu estou ciente de que a após o recebimento da skin terei que aguardar por 7 (sete) dias para
+                        realizar outra troca com a skin adquirida nesta transação. Confirmo também que estou fornecendo, através do meu 
+                        perfil na MVP Skins, um trade link válido e atualizado para o recebimento da skin. 
                     </p>
 
                     <Form>
