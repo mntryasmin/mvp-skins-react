351e4d2f885dcbd8042e9f233fcf4a133ba0eca0
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-11-18 18:53:28 -0300

    Correção do impedimento do esqueci a senha

--- src/components/micro/LoginModal/Collapse/Collapse.js
+++ src/components/micro/LoginModal/Collapse/Collapse.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Collapse.css'
-import { Button, Collapse, Container, Form, FormGroup, Row } from 'react-bootstrap'
+import { Button, Collapse, Container, Form, FormGroup, Row, FloatingLabel } from 'react-bootstrap'
 import LoginForm from '../../../macro/Forms/Login/LoginForm';
 import ButtonCustom from '../../Button/Button.js'
 import axios from 'axios';
@@ -10,67 +10,53 @@ function Example() {
 
     const [Validation, setValidation] = useState('')
 
-    const URLEMAIL = "http://localhost:8080/cliente/email/"
     const URL = "http://localhost:8080/authenticate/esqueci-minha-senha"
     const [email, setEmail] = useState('')
-    const [client, setClient] = useState('')
 
     const submit = (event) => {
         event.preventDefault()
-        if (email == '' ) {
+        if (email == '') {
             setValidation('O campo de E-mail não pode estar vazio')
         } else {
-            checkEmail()
+            sendEmail()
         }
-
     }
 
     const sendEmail = () => {
-        axios.post(`${URL}`, email).then(async () => {
-            console.log(email)
+        axios.post(`${URL}`, {email: email}).then((response) => {
+            setValidation('O e-mail foi enviado com sucesso. Cheque sua caixa de spam para prosseguir com a redefinição da sua senha')
+        }).catch((error) => {
+            setValidation('Esse e-mail não existe no nosso banco de dados, verifique se você digitou o e-mail corretamente')
         })
     }
-
-    const checkEmail = () => {
-        axios.get(`${URLEMAIL}` + email ).then(async (response) => {
-            const cliente = await response.data
-            console.log(cliente)
-            if (cliente != ''){
-                sendEmail()
-            }
-            else{
-                setValidation('Esse e-mail não existe no nosso banco de dados, verifique se você digitou o e-mail corretamente')
-            }
-        })
-    }
-    
 
     return (
         <>
-            <Button 
+            <Button
                 onClick={() => setOpen(!open)}
                 aria-controls="collapse-reset-password"
                 aria-expanded={open} className="links-login m-0 p-0"
             >
                 Esqueci a senha
             </Button>
-            
+
             <Collapse in={open}>
 
                 <Container id="collapse-reset-password" className="card card-body redefinir-senha">
                     <div className='m-3'>
-                    Esqueceu sua senha? Fique tranquilo, digite seu e-mail aqui em baixo para te
-                    enviarmos um e-mail com o link para redefinição da sua senha:
-                    <p></p>
-                    <div className='validation'>
-                    {Validation}
-                    </div>
-                    <Form>
-                        <FormGroup className="d-flex-justify-content-start">
-                            <LoginForm Email emailValue={email} onclick={(event) => setValidation('')} onchange={(event) => { setEmail(event.target.value); } }/>
-                            <ButtonCustom class='btn-primary-mvp layout-btn-forgot-password' label='enviar' onclick={(event) => {submit(event)}}/>
-                        </FormGroup>
-                    </Form>
+                        Esqueceu sua senha? Fique tranquilo, digite seu e-mail aqui em baixo para te
+                        enviarmos um e-mail com o link para redefinição da sua senha:
+                        <p></p>
+                        <div className='validation'>
+                            {Validation}
+                        </div>
+                        <Form>
+                            <FormGroup className="d-flex-justify-content-start">
+    
+                                <LoginForm Email emailValue={email} onclick={(event) => setValidation('')} onchange={(event) => { setEmail(event.target.value); }} />
+                                <ButtonCustom class='btn-primary-mvp layout-btn-forgot-password' label='enviar' onclick={(event) => { submit(event) }} />
+                            </FormGroup>
+                        </Form>
                     </div>
                 </Container>
             </Collapse>
