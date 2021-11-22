999fbf1650c129436e17b40d6c3a16566c343edf
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-11-21 23:25:21 -0300

    Atualização na Validação do cartão de crédito (Agora só pode clicar no finalizar compra se os termos forem aceitos)

--- src/components/micro/Checkout/PaymentCreditCard/PaymentCreditCard.js
+++ src/components/micro/Checkout/PaymentCreditCard/PaymentCreditCard.js
@@ -18,12 +18,16 @@ function PaymentCreditCard(props) {
     const [cvv, setCvv] = useState('')
     const [cpf, setCpf] = useState('')
     const [installments, setInstallments] = useState('')
-
+    const [dtCard, setDtCard] = useState('')
+    
+    const totalValue = props.vlTotal
+    
     const card = {name : name,
         cardNumber : cardNumber,
         cvv : cvv,
         cpf : cpf,
-        installments : installments
+        installments : installments,
+        dtCard : dtCard
     }
 
     const maskOnlyLetters = (value) => {
@@ -38,14 +42,13 @@ function PaymentCreditCard(props) {
             .replace(/(\d{4})(\d{0})(\d)/, "$1");
     };
 
-    const Valide = (value) => {
-        if (/^\d\D{3,4}$/.test(cvv)) {
-            return value
-        } else {
-            value = 'CVV inválido'
-            return value
-        }
-    }
+    const maskDate = (value) => {
+        return value
+          .replace(/\D/g, "")
+          .replace(/(\d{2})(\d)/, "$1/$2")
+          .replace(/(\d{2})(\d)/, "$1/$2")
+          .replace(/(\d{4})(\d)/, "$1");
+      };
 
     const maskCPF = (value) => {
         return value
@@ -55,6 +58,12 @@ function PaymentCreditCard(props) {
             .replace(/(\d{3})(\d{1,2})/, "$1-$2")
             .replace(/(-\d{2})\d+?$/, "$1");
     };
+
+    const maskCVV = (value) => {
+        return value
+        .replace(/\D/g, "")
+        .replace(/(\d{3})(\d{0})(\d)/, "$1")
+    }
 
 
 
@@ -76,33 +85,33 @@ function PaymentCreditCard(props) {
 
             <Row>
                 <Form.Label className="mt-3"> Número do cartão </Form.Label>
-                <Form.Control type="text" name="number" placeholder="Digite o número do cartão" value={cardNumber} onChange={(event) => { setCardNumber(maskCard(event.target.value)); console.log(cardNumber) }} />
+                <Form.Control type="text" name="number" placeholder="Digite o número do cartão" value={cardNumber} onChange={(event) => { setCardNumber(maskCard(event.target.value));  props.func(card) }} />
             </Row>
 
             <Row className="px-0 payment-span">
                 <Col className="mx-0 px-0 col-7">
                     <Form.Label className="px-2 mt-3"> Validade(MM/AA) </Form.Label>
-                    <Form.Control type="date" name="validade" />
+                    <Form.Control type="text" name="validade" placeholder='DD/MM/AAAA' value={dtCard} onChange={(event) => {setDtCard(maskDate(event.target.value)); props.func(card)} }/>
                 </Col>
 
                 <Col className="col-4">
                     <Form.Label className="mt-3"> CVV </Form.Label>
-                    <Form.Control type="text" name="cvv" placeholder="Digite o CVV" value={cvv} onChange={(event) => { setCvv(event.target.value) }} />
+                    <Form.Control type="text" name="cvv" placeholder="Digite o CVV" value={cvv} onChange={(event) => { setCvv(maskCVV(event.target.value)); props.func(card) }} />
                 </Col>
             </Row>
 
             <Row>
                 <Form.Label className="mt-3"> CPF do titular </Form.Label>
-                <Form.Control type="text" name="cpf" placeholder="Digite o CPF" value={cpf} onChange={(event) => { setCpf(maskCPF(event.target.value)) }} />
+                <Form.Control type="text" name="cpf" placeholder="Digite o CPF" value={cpf} onChange={(event) => { setCpf(maskCPF(event.target.value)); props.func(card) }} />
             </Row>
 
             <Row className="px-0 payment-form">
                 <Form.Label className="mt-3"> Parcelas </Form.Label>
-                <Form.Select aria-label="parcelas">
+                <Form.Select aria-label="parcelas" onChange={(event) => {setInstallments(event.target.value); props.func(card) }}>
                     <option value="0">Selecione as parcelas</option>
-                    <option value="1">1 x de R$ XX,XX sem juros</option>
-                    <option value="2">2 x de R$ XX,XX com juros</option>
-                    <option value="3">3 x de R$ XX,XX com juros</option>
+                    <option value="1">1 x de R$ {(totalValue / 1).toFixed(2)} sem juros</option>
+                    <option value="2">2 x de R$ {(totalValue / 2).toFixed(2)} com juros</option>
+                    <option value="3">3 x de R$ {(totalValue / 3).toFixed(2)} com juros</option>
                 </Form.Select>
             </Row>
         </Form>
