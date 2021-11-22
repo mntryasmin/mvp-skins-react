8c87eb56458c92c7a93b27ad6eb7dbbf1049fbd0
yasminmq <ymonteiroq@gmail.com>
2021-11-22 11:27:00 -0300

    Correções dos bugs em Segurança e Histórico de pedidos

--- src/components/macro/Dashboard/OrderHistory/OrderHistory.js
+++ src/components/macro/Dashboard/OrderHistory/OrderHistory.js
@@ -18,9 +18,8 @@ export default class OrderHistory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            requests: [],
             products: [],
-            show: false,
+            requests: [],
         };
         this.client = {
             codigoCliente: '',
@@ -37,24 +36,39 @@ export default class OrderHistory extends Component {
     }
 
     componentDidMount() {
-        // this.handleClient();
-        // this.handleRequests();
+        this.handleClient();
+        this.handleRequests();
+
     }
 
     handleClient() {
         this.client = JSON.parse(localStorage.getItem("client"));
+        console.log(this.client);
     }
 
     handleRequests() {
-        axios.get(`http://localhost:8080/pedidos/order-history/` + this.client.codigoCliente)
+        axios.get(`http://localhost:8080/pedidos/order-history/${this.client.codigoCliente}`, {headers: {
+            Authorization: localStorage.getItem('Authorization')}})
             .then((response) => {
-                this.setState({ requests: response.data });
+                this.setState({requests: response.data});
             })
             .catch((erro) => {
                 console.log("Não foi possível buscar os pedidos do cliente: " + erro)
             }
             );
     }
+
+    handleProducts(id) {
+        axios.get(`http://localhost:8080/itens-pedido/${id}`, {headers: {
+            Authorization: localStorage.getItem('Authorization')}})
+            .then((response) => {
+                this.setState({ products: response.data });
+            })
+            .catch((erro) => {
+                console.log("Ocorreu um erro " + erro)
+            });
+    }
+
 
     handleStatusRequests(statusRequest) {
         if (statusRequest == 0) {
@@ -64,8 +78,32 @@ export default class OrderHistory extends Component {
         }
     }
 
+    handleMapProducts(id) {
+        // this.handleProducts(id);
+        console.log(this.state.products);
+        
+        return this.state.products.map(
+            (product) =>
+                <>
+                    <Nav className="row list-group request-items-style py-1" defaultActiveKey="/home" as="ul">
+                        <Col className="col-4" >
+                            <Nav.Item as="li"><Image class="request-item-image" url={product.id.produto.urlImagem} /></Nav.Item>
+                        </Col>
+
+                        <Col className="col-5" >
+                            <Nav.Item as="li"> {product.id.produto.descricao} </Nav.Item>
+                        </Col>
+
+                        <Col className="col-3" >
+                            <Nav.Item as="li"> R$ {(product.valorLiquido.toFixed(2)).toString().replace(".", ",")} </Nav.Item>
+                        </Col>
+                    </Nav>
+                </>
+        )
+    }
+
     handleShowRequests() {
-        this.handleRequests();
+        // this.handleRequests();
         return this.state.requests.map(
             (request) =>
                 <>
@@ -91,14 +129,25 @@ export default class OrderHistory extends Component {
                         </Col>
 
                         <Col className="col-2" >
-                            <Button variant="primary" className="me-2 request-button">
+                            <Button variant="primary" onClick={() => {this.handleProducts(request.id)}} className="me-2 request-button">
                                 <img className="arrow" src={cursor} />
                             </Button>
                         </Col>
+                    </Nav>
+                    <Nav className="row list-group py-2 card-caption-mvp" defaultActiveKey="/home" as="ul">
+                        <Col className="col-4" >
+                            <Nav.Item as="li"> Produto </Nav.Item>
+                        </Col>
 
-                        <Modal show={this.state.show} onHide={this.setState({ show: false })}>
-                        </Modal>
+                        <Col className="col-5">
+                            <Nav.Item as="li"> Descrição </Nav.Item>
+                        </Col>
 
+                        <Col className="col-3 requests-resp">
+                            <Nav.Item as="li"> Preço </Nav.Item>
+                        </Col>
+
+                        {this.handleMapProducts(request.id)}
                     </Nav>
                 </>
         )
@@ -130,7 +179,10 @@ export default class OrderHistory extends Component {
                             <Nav.Item as="li" className=""> Abrir </Nav.Item>
                         </Col>
                     </Nav>
-                    {this.handleShowRequests()}
+
+        {this.handleShowRequests()}
+
+
                 </Container>
             </>
         )
