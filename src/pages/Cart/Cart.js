061ea8f2ea97eac6c1362e8b486f85c69d8b2be3
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-11-24 16:30:12 -0300

    Atualização da tarefa do cupom de desconto

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -10,34 +10,35 @@ import './Cart.css'
 import DiscountCoupon from '../../components/micro/Cart/DiscountCoupon/DiscountCoupon'
 import ProductListCart from '../../components/micro/Cart/CartItemsList/CartItemsList'
 import Button from '../../components/micro/Button/Button'
+import axios from 'axios'
 
 function Cart(props) {
     const [price, setPrice] = useState(0.0)
 
     let finalPrice = 0;
-    function getTotalPrice(totalPrice){
-        finalPrice = (finalPrice+totalPrice);
+    function getTotalPrice(totalPrice) {
+        finalPrice = (finalPrice + totalPrice);
         //Está duplicando o valor total por algum motivo desconhecido
-        setPrice(finalPrice/2);
+        setPrice(finalPrice / 2);
         console.log(finalPrice)
     }
 
-    function createOrder(){
+    function createOrder() {
         const client = JSON.parse(localStorage.getItem("client"))
         const order = {
-            cliente : client,
-            formaPagamento : {
-                id : 1
+            cliente: client,
+            formaPagamento: {
+                id: 1
             },
-            descontoProduto : 0.0,
-            valorBruto : price
+            descontoProduto: 0.0,
+            valorBruto: price
         }
 
         var orderString = JSON.stringify(order)
         localStorage.setItem("order", orderString)
-        window.location.href='http://localhost:3000/checkout'
+        window.location.href = 'http://localhost:3000/checkout'
     }
-    
+
 
     return (
         <>
@@ -46,7 +47,7 @@ function Cart(props) {
                 </Col>
 
                 <Container xs={12} sm={12} md={12} lg={8} xl={8} className="mx-4 my-5 pt-3 cart-container">
-                    <ProductListCart functionTotalPrice={getTotalPrice}/>
+                    <ProductListCart functionTotalPrice={getTotalPrice} />
 
                     <Container className="py-1 my-4 price">
                         <Col className="col-7 py-2 px-1 mt-2 discount-coupon-cart">
@@ -55,13 +56,13 @@ function Cart(props) {
 
                         <Row className="col-5 p-0 mt-2 price-cart">
                             <Col className="cart-values">
-                                <p className="m-0"> Cupom </p>
-                                <p className="m-0 mt-1">R$  </p>
+                                <DiscountCoupon boxDiscount price={price} />
                             </Col>
                             <Col className="cart-values">
                                 <p className="m-0"> Subtotal </p>
-                                <p className="m-0 mt-1"> {price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} </p>
+                                <p className="m-0 mt-1"> {price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} </p>
                             </Col>
+
                         </Row>
                     </Container>
 
@@ -69,8 +70,8 @@ function Cart(props) {
                         <Button label="Continuar comprando" route="/home" class="btn-secundary-mvp" navigation></Button>
 
                         <Button label="Finalizar compra"
-                        class="btn-primary-mvp" 
-                        onclick={()=>createOrder()}></Button>
+                            class="btn-primary-mvp"
+                            onclick={() => createOrder()}></Button>
                     </Container>
                 </Container>
             </Container>
