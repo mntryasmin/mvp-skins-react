b1606e6e98a2f601e3b60dfcac8fdc9505376489
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-11-25 16:12:36 -0300

    Atualização da validação do cartão de credito

--- src/components/micro/Checkout/PaymentCreditCard/PaymentCreditCard.js
+++ src/components/micro/Checkout/PaymentCreditCard/PaymentCreditCard.js
@@ -18,18 +18,19 @@ function PaymentCreditCard(props) {
     const [installments, setInstallments] = useState('')
     const [dtCard, setDtCard] = useState('')
     const [flag, setFlag] = useState('')
-   
-    
-    
+
+
+
     const totalValue = props.vlTotal
-    
-    const card = {name : name,
-        cardNumber : cardNumber,
-        cvv : cvv,
-        cpf : cpf,
-        installments : installments,
-        dtCard : dtCard,
-        flag:flag,
+
+    const card = {
+        name: name,
+        cardNumber: cardNumber,
+        cvv: cvv,
+        cpf: cpf,
+        installments: installments,
+        dtCard: dtCard,
+        flag: flag,
 
     }
 
@@ -64,16 +65,16 @@ function PaymentCreditCard(props) {
 
     const maskCVV = (value) => {
         return value
-        .replace(/\D/g, "")
-        .replace(/(\d{3})(\d{0})(\d)/, "$1")
+            .replace(/\D/g, "")
+            .replace(/(\d{3})(\d{0})(\d)/, "$1")
     };
-    
-    const maskFlag = (value) =>{
+
+    const maskFlag = (value) => {
         return value
-        .replace(/^4[0-9]{12}(?:[0-9]{3})/)
-        .replace(/^5[1-5][0-9]{14}/)
+            .replace(/^4[0-9]{12}(?:[0-9]{3})/)
+            .replace(/^5[1-5][0-9]{14}/)
     };
-    
+
     return (
         <Form className="payment-form p-0 m-0">
             <div className='validation-card'>
@@ -89,15 +90,22 @@ function PaymentCreditCard(props) {
                     placeholder="Digite seu nome" />
             </Row>
 
-            <Row>
-                <Form.Label className="mt-3"> Número do cartão </Form.Label>
-                <Form.Control type="text" name="number" placeholder="Digite o número do cartão" value={cardNumber} onChange={(event) => { setCardNumber(maskCard(event.target.value)); props.func(card) }} />
+            <Row className="px-0 payment-span">
+                <Col md={7} className='mx-1 p-0'>
+                    <Form.Label className="mt-3"> Número do cartão </Form.Label>
+                    <Form.Control type="text" name="number" placeholder="Digite o número do cartão" value={cardNumber} onChange={(event) => { setCardNumber(maskCard(event.target.value)); props.func(card) }} />
+                </Col>
+                <Col md={4} className='px-0 mx-0'>
+                    <Form.Label className="mt-3 px-2"> Bandeira </Form.Label>
+                    <Form.Control disabled type="text" name="number" value={flag} onChange={(event) => { setCardNumber(maskCard(event.target.value)); props.func(card) }} />
+                </Col>
+
             </Row>
 
             <Row className="px-0 payment-span">
                 <Col className="mx-0 px-0 col-7">
                     <Form.Label className="px-2 mt-3"> Validade(MM/AAAA) </Form.Label>
-                    <Form.Control type="text" name="validade" placeholder='MM/AAAA' value={dtCard} onChange={(event) => {setDtCard(maskDate(event.target.value)); props.func(card)} }/>
+                    <Form.Control type="text" name="validade" placeholder='MM/AAAA' value={dtCard} onChange={(event) => { setDtCard(maskDate(event.target.value)); props.func(card) }} />
                 </Col>
 
                 <Col className="col-4">
@@ -113,7 +121,7 @@ function PaymentCreditCard(props) {
 
             <Row className="px-0 payment-form">
                 <Form.Label className="mt-3"> Parcelas </Form.Label>
-                <Form.Select aria-label="parcelas" onClick={(event) => {setInstallments(event.target.value); props.func(card) }}>
+                <Form.Select aria-label="parcelas" onClick={(event) => { setInstallments(event.target.value); props.func(card) }}>
                     <option value="0">Selecione as parcelas</option>
 
                     <option value="1">1 x de R$ {(totalValue / 1).toFixed(2).replace(".", ",")} sem juros</option>
@@ -122,9 +130,9 @@ function PaymentCreditCard(props) {
                 </Form.Select>
             </Row>
         </Form>
-        
+
     )
-    
+
 }
 
 export default PaymentCreditCard
