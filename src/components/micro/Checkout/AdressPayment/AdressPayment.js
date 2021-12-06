01762fcf693f8fc584abaa1dd0d350a6cf68eceb
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-12-06 12:14:38 -0300

    Resolvendo falhas no site

--- src/components/micro/Checkout/AdressPayment/AdressPayment.js
+++ src/components/micro/Checkout/AdressPayment/AdressPayment.js
@@ -20,7 +20,7 @@ export default function AdressPayment(props) {
 
     const [validation, setValidation] = useState('')
     const [classTerm, setClassTerm] = useState('')
-    const [cep, setcep] = useState('');
+    const [cep, setCep] = useState('');
     const [logradouro, setLogradouro] = useState('');
     const [numero, setNumero] = useState('');
     const [complemento, setComplemento] = useState('');
@@ -35,7 +35,7 @@ export default function AdressPayment(props) {
 
     // var lastRequest = null;
 
-    async function getRequestsClient() {
+    async function GetRequestsClient() {
         let response = await axios.get(
             `http://localhost:8080/pedidos/order-history/${cliente.codigoCliente}`, {
             headers: {
@@ -67,197 +67,198 @@ export default function AdressPayment(props) {
                 setAdressResult(addressFound);
                 console.log('ENDEREÇO: V')
                 console.log(responseAdress.data.cep)
-            } 
+            }
         } else {
             setAdressResult(adressNotFound);
         }
 
 
-    //Recupera o endereço através do CEP (usando a API do viacep)
-    function consultarcep(cep){
-        axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-        .then((response)=>{
-            // setEndereco(response.data)
-            if(response.data.erro){
-                setLogradouro('')
-                setBairro('')
-                setCidade('')
-                setEstado('')
-                validateCep(false)
-            } else {
-                setcep(response.data.cep)
-                setLogradouro(response.data.logradouro)
-                setBairro(response.data.bairro)
-                setCidade(response.data.localidade)
-                setEstado(response.data.uf)
-                validateCep(true)
-            }
-            
-        })
-        .catch((error)=>{
-            console.log("Ocorreu um erro ao consultar o cep "+ error)
-            setLogradouro('')
-            setBairro('')
-            setCidade('')
-            setEstado('')
-            validateCep(false)
-        })
-    }
+        //Recupera o endereço através do CEP (usando a API do viacep)
+        function consultarcep(cep) {
+            axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+                .then((response) => {
+                    // setEndereco(response.data)
+                    if (response.data.erro) {
+                        setLogradouro('')
+                        setBairro('')
+                        setCidade('')
+                        setEstado('')
+                        validateCep(false)
+                    } else {
+                        setCep(response.data.cep)
+                        setLogradouro(response.data.logradouro)
+                        setBairro(response.data.bairro)
+                        setCidade(response.data.localidade)
+                        setEstado(response.data.uf)
+                        validateCep(true)
+                    }
 
-    useEffect(() => {
-        getRequestsClient();
-    }, []);
+                })
+                .catch((error) => {
+                    console.log("Ocorreu um erro ao consultar o cep " + error)
+                    setLogradouro('')
+                    setBairro('')
+                    setCidade('')
+                    setEstado('')
+                    validateCep(false)
+                })
+        }
 
-    const maskCEP = (value) => {
-        return value
-            .replace(/\D/g, "")
-            .replace(/(\d{5})(\d{1,2})/, "$1-$2")
-            .replace(/(-\d{3})\d+?$/, "$1");
-    };
+        useEffect(() => {
+            GetRequestsClient();
+        }, []);
 
-    const maskTextNumber = (value) => {
-        return value.replace(/[!@#¨$%^&*)}",|?;{(+=._-]+/g, "");
-    }
-
-    const maskNumber = (value) => {
-        return value.replace(/[!@#¨$%^&*)}'",|?;{(+=._-]+/g, "");
-    }
-
-    const maskUF = (value) => {
-        return value
-            .replace(/[!@#¨$%^&*)}'",|?;{(+=._-]+/g, "")
-            .replace(/(\d{2})(\d{0})(\d)/, "$1")
-    }
-
-    const changeDisabled = () => {
-        setDisabled(false);
-    }
-
-    function validAdress() {
-        let endereco = {
-            cep: cep,
-            logradouro: logradouro,
-            numero: numero,
-            complemento: complemento,
-            bairro: bairro,
-            cidade: cidade,
-            estado: estado,
+        const maskCEP = (value) => {
+            return value
+                .replace(/\D/g, "")
+                .replace(/(\d{5})(\d{1,2})/, "$1-$2")
+                .replace(/(-\d{3})\d+?$/, "$1");
         };
 
-        console.log(endereco);
-        setDisabled(true);
-        props.func(endereco);
-    }
-
-    function validateCep(cepBoolean){
-        if(cepBoolean == false){
-            setValidation('CEP inválido')
-            setClassTerm('validation-term p-2')
-        } else {
-            setValidation('')
-            setClassTerm('')
+        const maskTextNumber = (value) => {
+            return value.replace(/[!@#¨$%^&*)}",|?;{(+=._-]+/g, "");
         }
+
+        const maskNumber = (value) => {
+            return value.replace(/[!@#¨$%^&*)}'",|?;{(+=._-]+/g, "");
+        }
+
+        const maskUF = (value) => {
+            return value
+                .replace(/[!@#¨$%^&*)}'",|?;{(+=._-]+/g, "")
+                .replace(/(\d{2})(\d{0})(\d)/, "$1")
+        }
+
+        const changeDisabled = () => {
+            setDisabled(false);
+        }
+
+        function validAdress() {
+            let endereco = {
+                cep: cep,
+                logradouro: logradouro,
+                numero: numero,
+                complemento: complemento,
+                bairro: bairro,
+                cidade: cidade,
+                estado: estado,
+            };
+
+            console.log(endereco);
+            setDisabled(true);
+            props.func(endereco);
+        }
+
+        function validateCep(cepBoolean) {
+            if (cepBoolean == false) {
+                setValidation('CEP inválido')
+                setClassTerm('validation-term p-2')
+            } else {
+                setValidation('')
+                setClassTerm('')
+            }
+        }
+
+        return (
+            <>
+                <Form className="col-12 form-endereco-cobranca">
+                    <p>{adressResult}</p>
+                    <div className={classTerm}>
+                        {validation}
+                    </div>
+                    <Col className="col-12">
+                        <Form.Label className="mt-3"> CEP </Form.Label>
+                        <Form.Control disabled={disabled} className="input-disabled" type="text" name="cep" placeholder="Digite o CEP" value={cep}
+                            onChange={(event) => {
+                                setCep(maskCEP(event.target.value));
+                            }} />
+                    </Col>
+
+                    <Col className="col-12">
+                        <Form.Label className="mt-3"> Logradouro </Form.Label>
+                        <Form.Control disabled={disabled} className="input-disabled" type="text" name="logradouro" placeholder="Digite o nome da rua" value={logradouro}
+
+                            onChange={(event) => {
+                                setLogradouro(maskTextNumber(event.target.value));
+                            }} />
+                    </Col>
+
+                    <Col className="col-3">
+                        <Form.Label className="mt-3"> Nº </Form.Label>
+                        <Form.Control disabled={disabled} className="input-disabled" type="text" name="numero" placeholder="Digite o nº da residência" value={numero}
+                            onChange={(event) => {
+                                setNumero(maskNumber(event.target.value));
+                            }} />
+                    </Col>
+
+                    <Col className="col-8">
+                        <Form.Label className="mt-3"> Complemento </Form.Label>
+                        <Form.Control disabled={disabled} className="input-disabled" type="text" name="complemento" placeholder="Digite o complemento" value={complemento}
+                            onChange={(event) => {
+                                setComplemento(maskTextNumber(event.target.value));
+                            }} />
+                    </Col>
+
+                    <Col className="col-12">
+                        <Form.Label className="mt-3"> Bairro</Form.Label>
+                        <Form.Control disabled={disabled} className="input-disabled" type="text" name="bairro" placeholder="Digite o bairro" value={bairro}
+
+                            onChange={(event) => {
+                                setBairro(maskTextNumber(event.target.value));
+                            }} />
+                    </Col>
+
+                    <Col className="col-12">
+                        <Form.Label className="mt-3"> Cidade </Form.Label>
+                        <Form.Control disabled={disabled} className="input-disabled" type="text" name="cidade" placeholder="Digite a cidade" value={cidade}
+
+                            onChange={(event) => {
+                                setCidade(maskTextNumber(event.target.value));
+                            }} />
+                    </Col>
+
+                    <Col className="col-12">
+                        <Form.Label className="mt-3"> Estado </Form.Label>
+                        <Form.Select disabled={disabled} className="input-disabled" aria-label="Default select example" value={estado} onChange={(event) => {
+                            setEstado(maskUF(event.target.value))
+                        }}>
+                            <option>Estado</option>
+                            <option value="AC">Acre</option>
+                            <option value="AL">Alagoas</option>
+                            <option value="AP">Amapá</option>
+                            <option value="AM">Amazonas</option>
+                            <option value="BA">Bahia</option>
+                            <option value="CE">Ceará</option>
+                            <option value="DF">Distrito Federal</option>
+                            <option value="ES">Espírito Santo</option>
+                            <option value="GO">Goiás</option>
+                            <option value="MA">Maranhão</option>
+                            <option value="MT">Mato Grosso</option>
+                            <option value="MS">Mato Grosso do Sul</option>
+                            <option value="MG">Minas Gerais</option>
+                            <option value="PA">Pará</option>
+                            <option value="PB">Paraíba</option>
+                            <option value="PR">Paraná</option>
+                            <option value="PE">Pernambuco</option>
+                            <option value="PI">Piauí</option>
+                            <option value="RJ">Rio de Janeiro</option>
+                            <option value="RN">Rio Grande do Norte</option>
+                            <option value="RS">Rio Grande do Sul</option>
+                            <option value="RO">Rondônia</option>
+                            <option value="RR">Roraima</option>
+                            <option value="SC">Santa Catarina</option>
+                            <option value="SP">São Paulo</option>
+                            <option value="SE">Sergipe</option>
+                            <option value="TO">Tocantins</option>
+                        </Form.Select>
+                    </Col>
+                </Form>
+
+                <Col className="adress-payment-buttons">
+                    <Button label="Alterar" onclick={() => changeDisabled()} class="mt-3 btn-mvp btn-mvp-purple-solid col-4" />
+                    <Button label="Salvar" onclick={() => validAdress()} class="mt-3 btn-mvp btn-mvp-orange-solid col-4" />
+                </Col>
+            </>
+        )
     }
-
-    return (
-        <>
-            <Form className="col-12 form-endereco-cobranca">
-                <p>{adressResult}</p>
-                <div className={classTerm}>
-                    {validation}
-                </div>
-                <Col className="col-12">
-                    <Form.Label className="mt-3"> CEP </Form.Label>
-                    <Form.Control disabled={disabled} className="input-disabled" type="text" name="cep" placeholder="Digite o CEP" value={cep}
-                        onChange={(event) => {
-                            setCep(maskCEP(event.target.value));
-                        }} />
-                </Col>
-
-                <Col className="col-12">
-                    <Form.Label className="mt-3"> Logradouro </Form.Label>
-                    <Form.Control disabled={disabled} className="input-disabled" type="text" name="logradouro" placeholder="Digite o nome da rua" value={logradouro}
-
-                        onChange={(event) => {
-                            setLogradouro(maskTextNumber(event.target.value));
-                        }} />
-                </Col>
-
-                <Col className="col-3">
-                    <Form.Label className="mt-3"> Nº </Form.Label>
-                    <Form.Control disabled={disabled} className="input-disabled" type="text" name="numero" placeholder="Digite o nº da residência" value={numero}
-                        onChange={(event) => {
-                            setNumero(maskNumber(event.target.value));
-                        }} />
-                </Col>
-
-                <Col className="col-8">
-                    <Form.Label className="mt-3"> Complemento </Form.Label>
-                    <Form.Control disabled={disabled} className="input-disabled" type="text" name="complemento" placeholder="Digite o complemento" value={complemento}
-                        onChange={(event) => {
-                            setComplemento(maskTextNumber(event.target.value));
-                        }} />
-                </Col>
-
-                <Col className="col-12">
-                    <Form.Label className="mt-3"> Bairro</Form.Label>
-                    <Form.Control disabled={disabled} className="input-disabled" type="text" name="bairro" placeholder="Digite o bairro" value={bairro}
-
-                        onChange={(event) => {
-                            setBairro(maskTextNumber(event.target.value));
-                        }} />
-                </Col>
-
-                <Col className="col-12">
-                    <Form.Label className="mt-3"> Cidade </Form.Label>
-                    <Form.Control disabled={disabled} className="input-disabled" type="text" name="cidade" placeholder="Digite a cidade" value={cidade}
-
-                        onChange={(event) => {
-                            setCidade(maskTextNumber(event.target.value));
-                        }} />
-                </Col>
-
-                <Col className="col-12">
-                    <Form.Label className="mt-3"> Estado </Form.Label>
-                    <Form.Select disabled={disabled} className="input-disabled" aria-label="Default select example" value={estado} onChange={(event) => {
-                        setEstado(maskUF(event.target.value))
-                    }}>
-                        <option>Estado</option>
-                        <option value="AC">Acre</option>
-                        <option value="AL">Alagoas</option>
-                        <option value="AP">Amapá</option>
-                        <option value="AM">Amazonas</option>
-                        <option value="BA">Bahia</option>
-                        <option value="CE">Ceará</option>
-                        <option value="DF">Distrito Federal</option>
-                        <option value="ES">Espírito Santo</option>
-                        <option value="GO">Goiás</option>
-                        <option value="MA">Maranhão</option>
-                        <option value="MT">Mato Grosso</option>
-                        <option value="MS">Mato Grosso do Sul</option>
-                        <option value="MG">Minas Gerais</option>
-                        <option value="PA">Pará</option>
-                        <option value="PB">Paraíba</option>
-                        <option value="PR">Paraná</option>
-                        <option value="PE">Pernambuco</option>
-                        <option value="PI">Piauí</option>
-                        <option value="RJ">Rio de Janeiro</option>
-                        <option value="RN">Rio Grande do Norte</option>
-                        <option value="RS">Rio Grande do Sul</option>
-                        <option value="RO">Rondônia</option>
-                        <option value="RR">Roraima</option>
-                        <option value="SC">Santa Catarina</option>
-                        <option value="SP">São Paulo</option>
-                        <option value="SE">Sergipe</option>
-                        <option value="TO">Tocantins</option>
-                    </Form.Select>
-                </Col>
-            </Form>
-
-            <Col className="adress-payment-buttons">
-                <Button label="Alterar" onclick={() => changeDisabled()} class="mt-3 btn-mvp btn-mvp-purple-solid col-4" />
-                <Button label="Salvar" onclick={() => validAdress()} class="mt-3 btn-mvp btn-mvp-orange-solid col-4" />
-            </Col>
-        </>
-    )
 }
